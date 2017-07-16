4eaa877b05da6228de70042c417719c959039fee
timdpr <qotimsa@live.co.uk>
2017-07-16 21:31:19 +0100

    Add Algorithmic Crush

---
+++ hackerrank-algorithmic-crush/src/main/Solution.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * Completes hackerrank.com/challenges/crush in O(n+m) time
+ * rather than O(nm) time by using an endpoint array!
+ * 
+ * @author Timdpr
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int listSize = in.nextInt();
+        int operations = in.nextInt();
+        long[] endpointArray = new long[listSize+1];
+
+        for (int i=0; i<operations; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int k = in.nextInt();
+            endpointArray[a-1] += k;
+            endpointArray[b] -=k;
+        }        
+        long total = 0;
+        long max = 0;
+        for (int i=0; i<listSize+1; i++) {
+            total += endpointArray[i];
+            if (total > max) {
+                max = total;
+            }
+        }
+        System.out.println(max);
+        in.close();
+    }
+}
