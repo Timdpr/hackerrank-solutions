f62a4359852a8fae0054b9c232b49bde1fd6b86c
timdpr <qotimsa@live.co.uk>
2017-09-10 12:44:56 +0100

    Finish solution, refactor to use string etc

--- chef-and-pick-digit/src/main/Main.java
+++ chef-and-pick-digit/src/main/Main.java
@@ -1,55 +1,48 @@
 package main;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int testCases = in.nextInt();
+        int testCases = Integer.parseInt(in.nextLine());
+
         for (int testCase = 0; testCase < testCases; testCase++) {
-
-            int N = in.nextInt();
-
-            printList(checkNsPerms(N));
+            String N = in.nextLine(); // Use string - actual (often huge) numbers need not be read!
+            System.out.println(getLetters(N));
         }
     }
 
-    public static char[] checkNsPerms(int N) {
-        char[] results = new char[N*N];
-        int i = 0;
-
-        if (N < 17) {
-            return results;
-        }
-
-        int[] NPerms = getNPerms(N); // TODO: create method of enumerating all permutations of N etc...if they are all unique, good!
+    private static String getLetters(String N) {
+        StringBuilder result = new StringBuilder();
+        ArrayList<Integer> NPerms = getNPerms(N);
 
         for (int n : NPerms) {
             if (n >= 65 && n <= 90) { // if n in A-Z range
-                results[i] = ((char) n);
-                i++;
+                result.append((char) n);
             }
-            Arrays.sort(results);
-            return results;
         }
+        return result.toString();
     }
 
-    private static int[] getNPerms(int N) {
-        HashSet<Integer> result = new HashSet<>();
-        String NString = Integer.toString(N);
-        int i = 0;
-        for (char c : NString) {
+    private static ArrayList<Integer> getNPerms(String N) {
+        ArrayList<Integer> NNumbers = new ArrayList<>();
+        ArrayList<Integer> NPerms = new ArrayList<>();
 
+        for (int i = 0; i < N.length(); i++) {
+            NNumbers.add(Character.getNumericValue(N.charAt(i)));
         }
-    }
 
-    public static void printList(char[] list) {
-        for (char c : list) {
-            System.out.print(c);
+        Collections.sort(NNumbers);
+
+        for (int j=0; j<NNumbers.size(); j++) {
+            if (NNumbers.get(j) >= 6) {
+                for (int k=0; k<NNumbers.size(); k++) {
+                    if ( (k != j) && (!NPerms.contains(NNumbers.get(j) * 10 + NNumbers.get(k))) ) {
+                        NPerms.add(NNumbers.get(j) * 10 + NNumbers.get(k)); } } }
         }
-        System.out.println();
+        return NPerms;
     }
 }
